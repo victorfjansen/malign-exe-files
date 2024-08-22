3f114f4edf1d695cfa42692c47e30d502d4679f1
Victor Jansen <victorfjansen@gmail.com>
2024-08-22 14:45:37 -0300

    fix: acerta multi_label

--- scripts/multi_label_mnemonic_csv_generator.py
+++ scripts/multi_label_mnemonic_csv_generator.py
@@ -40,6 +40,15 @@ def disassemble_file(file_path):
     
     return filtered_disassembly
 
+def get_locally_most_common_sequences(disassembly):
+    filtered_disassembly = [] 
+    for instruction in disassembly:
+        instruction_mnemonic = instruction.split("\t")[0]
+        if any(item[0] == instruction_mnemonic for item in mnemonic_counter.most_common(31)):
+            filtered_disassembly.append(instruction_mnemonic)
+    
+    return filtered_disassembly
+
 def process_directory(input_directory, csv_output_path):
     # Open the CSV file for writing
     with open(csv_output_path, "w", newline="") as csvfile:
@@ -55,7 +64,8 @@ def process_directory(input_directory, csv_output_path):
                     try:
                         print(f"Processing {filename}...")
                         disassembled_code = disassemble_file(input_path)
-                        opcode_sequence = " ".join(disassembled_code)
+                        most_common_opcodes = get_locally_most_common_sequences(disassembled_code)
+                        opcode_sequence = " ".join(most_common_opcodes)
                     except:
                         continue
                     
